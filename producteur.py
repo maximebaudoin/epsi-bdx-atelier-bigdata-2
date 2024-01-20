fead890c4129ac099fce214ddd1f7576966bdf08
Maxime BAUDOIN <42619561+maximebaudoin@users.noreply.github.com>
2024-01-20 23:10:46 +0100

    Update producteur.py

--- producteur.py
+++ producteur.py
@@ -1,4 +1,6 @@
-import time, json, random
+import time
+import json
+import random
 import requests
 from kafka import KafkaProducer
 
@@ -8,30 +10,24 @@ minute = 55
 hour = 11
 
 while True:
-    response = requests.get(f"https://[API_URL]/?hour={hour}&minute={minute}")
+    response = requests.get(f"http://127.0.0.1:5001/api/?hour={hour}&minute={minute}")
     if response.status_code != 200:
         continue
     
-    datas = []
+    datas = {}
     
-    for table_line in response.json:
-        if table_line.line not in datas:
-            setattr(
-                datas,
-                table_line.line,
-                [
-                    table_line.retard
-                ]
-            )
+    for table_line in response.json():
+        if str(table_line["rs_sv_ligne_a"]) not in datas:
+            datas[str(table_line["rs_sv_ligne_a"])] = [table_line["retard"]]
         else:
-            datas[table_line.line].append(table_line.retard)
+            datas[str(table_line["rs_sv_ligne_a"])].append(table_line["retard"])
     
     for line, line_datas in datas.items():
         print(f"Envoi de : {line_datas} ({line})")
         producteur.send("retards", json.dumps(line_datas).encode(), line.encode())
-    
+
     time.sleep(1)
-    
+
     minute += 1
     hour += minute // 60
     minute %= 60
